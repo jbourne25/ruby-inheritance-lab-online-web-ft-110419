247da8fcbd1da2d27df1687b42efb0db7c7709f6
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-23 06:57:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  
+  @knowledge = []
+  
+  # def initialize(knowledge)
+  #   @knowledge << knowledge 
+  # end 
+  
+  
+  def learn(string)
+    student = Student.new(string)
+    Student.knowledge.push 
+  end 
+  
+  def knowledge
+    @knowledge   
+  end
 
 end
